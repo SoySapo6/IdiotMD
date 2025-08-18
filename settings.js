78e39645c67823e071c40a46d9de32b93509eb66
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-08-18 09:56:04 -0500

    Hecho por SoyMaycol

---
+++ settings.js
@@ -0,0 +1,89 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone' 
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.botNumber = '' 
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = [
+  ['573196588149', 'SoyMaycol', true],
+  ['180650938249287', 'SoyMaycol', true], 
+];
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.mods = []
+global.suittag = [] 
+global.prems = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.libreria = 'Baileys'
+global.baileys = 'V 6.7.17' 
+global.vs = '1.0.0'
+global.nameqr = 'IdiotMD'
+global.namebot = ''
+global.sessions = './MayBots/Principal'
+global.jadi = 'MayBots' 
+global.yukiJadibts = true
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = 'ᴵᵈⁱᵒᵗᴹᴰ'
+global.botname = '𝐈𝐝𝐢𝐨𝐭𝕄𝔻'
+global.wm = 'ＩｄｉｏｔＭＤ'
+global.author = '𝐈𝐝𝐢𝐨𝐭𝐌𝐃 ᴴᵉᶜʰᵒ ᵖᵒʳ ˢᵒʸᵐᵃʸᶜᵒˡ'
+global.dev = 'ᴴᵉᶜʰᵒ ᵖᵒʳ ˢᵒʸᵐᵃʸᶜᵒˡ'
+global.textbot = 'You Are An Idiot :)'
+global.etiqueta = '𝚂𝚘𝚢𝙼𝚊𝚢𝚌𝚘𝚕'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.moneda = 'IdiotCoins'
+global.welcom1 = 'Woah, Un nuevo integrante no?, Que interesante... Hey vamos usame.'
+global.welcom2 = 'Blah Blah Blah, Que pendejo el que se fue. Ahi le envio uj virus jeje'
+global.banner = 'https://files.catbox.moe/1b90ch.jpeg'
+global.avatar = 'https://files.catbox.moe/xcsmig.jpeg'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.gp1 = 'https://chat.whatsapp.com/KI2MyKoGWFV7ylP49ioLQ0?mode=ac_t'
+global.comunidad1 = 'https://chat.whatsapp.com/KqkJwla1aq1LgaPiuFFtEY'
+global.channel = 'https://whatsapp.com/channel/0029VayXJte65yD6LQGiRB0R'
+global.channel2 = 'https://whatsapp.com/channel/0029VayXJte65yD6LQGiRB0R'
+global.md = 'https://github.com/SoySapo6/IdiotMD'
+global.correo = 'soymaycol.cn@gmail.com'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.catalogo = fs.readFileSync('./src/catalogo.jpg');
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.ch = {
+ch1: '120363372883715167@newsletter'
+}
+global.multiplier = 60
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.cheerio = cheerio
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+global.moment = moment   
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'settings.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
